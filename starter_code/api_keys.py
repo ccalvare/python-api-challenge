af136e667821b676a0d2de6df067f1e20c0ff646
ccalvare <ccalvare@usc.edu>
2020-10-16 10:34:28 -0700

    google api code

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "03162c31b7b2d7317a1ee712e96e4175"
+weather_api_key = "6a0151f56150ae4493d6c0aa50748d0c"
 
 # Google API Key
 g_key = "AIzaSyAj_RKeUCWR72Sr6-jRpnNyNvy7GJ8CIWk"
