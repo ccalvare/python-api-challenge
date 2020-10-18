f394080775aaf2962c2e9b363e62b022002f79de
Carter Alvarez <69866942+ccalvare@users.noreply.github.com>
2020-10-18 13:28:19 -0700

    Update api_keys.py

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "03162c31b7b2d7317a1ee712e96e4175"
+weather_api_key = "message me"
 
 # Google API Key
-g_key = "AIzaSyAj_RKeUCWR72Sr6-jRpnNyNvy7GJ8CIWk"
+g_key = "message me"
